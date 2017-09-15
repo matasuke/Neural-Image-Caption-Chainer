d418b559517c5abb7e7ccbf08402ef56b4589d3e
matasukef <f-e@toki.waseda.jp>
2017-09-15 21:53:01 +0900

    fix it

--- src/preprocess_captions.py
+++ src/preprocess_captions.py
@@ -2,7 +2,7 @@ import os
 import json
 import argparse
 import numpy as np
-from numpy.random import shuffle
+from numpy import random
 
 
 class Tokenizer(object):
@@ -35,8 +35,8 @@ class Tokenizer(object):
         return self.segmenter(caption)
 
 
-def words2ids(tokens):
-    return [ word_ids[token] if token in word_ids else unknown_word for token in tokens ]
+def words2ids(tokens, word_ids):
+    return [ word_ids[token]['idx'] if token in word_ids else word_ids['<UNK>']['idx'] for token in tokens ]
 
 def loadpickle(p_file):
     with open(p_file, 'rb') as f:
@@ -75,14 +75,98 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
-
     tokenizer = Tokenizer(args)
 
     formatted_train = loadpickle(args.input_train)
     formatted_val = loadpickle(args.input_val)
 
-    list_sentences = {
-            k: {} for k in ['train', 'val', 'test']
-            }
+    train_data = []
+    val_data = []
+    test_data = []
 
-    list_img_ids 
+    val_img_num = int(len(formatted_val) * args.ratio)
+
+    #validation data and test data
+    for i, img in enumerate(tqdm(formatted_val)):
+        if 'tokenized_captions' in img:
+            for j, caption in enumerate(img['tokenized_captions']):
+                img[i]['tokenized_captions'][j] = caption.split()
+        else:
+            img[i]['tokenized_captions'] = []
+            for j, caption in enumerate(img['captions']):
+                img[i]['tokenized_captions'].append(tokenizer.pre_process(caption))
+
+    random.seed(0)
+    random.shuffle(formatted_val)
+    val_data.append(formatted_val[:val_img_num])
+    test_data.append(formatted_val[val_img_num:])
+    train_data.append(formatted_train)
+
+    img_idx = 0
+    caption_idx = 0
+    captions = []
+    images = []
+
+    word_counter = {}
+    word_ids = {}
+
+    for img in tqdm(formatted_train):
+        if 'tokenized_captions' in img:
+            for caption in enumerate(img['tokenized_captions']):
+                caption_tokens = ['<S>']
+                caption_tokens += caption.split()
+                caption_tokens.append('</S>')
+                captions.append({'img_idx': img_idx, 'caption': caption_tokens, 'caption_idx': caption_idx})
+                caption_idx += 1
+            #del img['captions']
+            #del img['tokenized_captions']
+            images.append({'file_path': img['file_path'], 'img_idx': img_idx})
+            img_idx += 1
+
+        else:
+            for caption in enumerate(img['captions']):
+                caption_tokens = ['<S>']
+                caption_tokens += tokenizer.pre_process(caption)
+                caption_tokens.append('</S>')
+                captions.append({'img_idx': img_idx, 'caption': caption_tokens, 'caption_idx': caption_idx})
+                caption_idx += 1
+            #del img['captions']
+            #del img['tokenized_captions']
+            images.append({'file_path': img['file_path'], 'img_idx': img_idx})
+            img_idx += 1
+    
+    #create vocabrary dictonary
+    for caption in tqdm(captions):
+        tokens = caption['caption']
+        
+        for token in tokens:
+            if token in word_counter:
+                word_counter[token] += 1
+            else:
+                word_counter[token] = 1
+
+    print('total distinct words:' len(word_counter))
+    print('top 30 frequent words:')
+    sorted_word = sorted(word_counter.items(), key=lambda x: x[1], reverse=True)
+    for word, freq in sorted_word[:20]:
+        print('{0} - {1}'.format(word, freq))
+
+    unknown = 0
+    for word, num in tqdm(word_counter.items()):
+        if num > args.off:
+            word_ids[word] = {'freq':num, 'idx':len(word_ids)}
+        else:
+            unknown += 1
+    word_ids['<UNK>'] = {'freq': unknown, 'idx': len(word_ids)}
+
+    print('total distinct words more than {0} : {1}', args.off, len(word_ids))
+
+    #encoding 
+    for caption in tqdm(captions):
+        caption['caption'] = words2ids(caption['caption'], word_ids)
+        #caption['encoded_tokenized_caption'] = words2ids(caption['caption'], word_ids)
+
+    output_dataset = {}
+    output_dataset['train'] = {'images': images, 'captions': captions, 'word_ids': word_ids}
+    output_dataset['val'] = val_data
+    output_dataset['test'] = test_data
