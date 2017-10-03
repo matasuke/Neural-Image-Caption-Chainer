23bd669d3a9846edfd2e3c9d28ddec86c4bec35e
matasukef <f-e@toki.waseda.jp>
2017-10-04 00:26:03 +0900

    fix it

--- download.sh
+++ download.sh
@@ -6,31 +6,30 @@ return_yes_or_no(){
         read answer
 
         case $answer in
-            yes)
-                break
+            [Yy]* )
+                return 0
                 ;;
-            no)
-                break
+            [Nn]* )
+                return 1
                 ;;
             *)
                 echo -e 'type yes or no\n'
                 ;;
         esac
     done
-
-    return $answer
 }
 
 
 echo 'Do you want to download pre-learned models(yes/no)?'
-model_download=return_yes_or_no
+model_download=`return_yes_or_no`
 echo 'Do you want to download images(yes/no)?'
-image_download=return_yes_or_no
+image_download=`return_yes_or_no`
 echo 'Do you want to download captions(yes/no)?'
-caption_download=return_yes_or_no
+caption_download=`return_yes_or_no`
 
+echo $model_download
 
-if [ $model_download == 'yes' ]; then
+if [ ! $model_download ]; then
     if [ ! -d models/cnn -o ! -d models/rnn ]; then
         mkdir --parents models/cnn models/rnn
     fi
@@ -39,7 +38,7 @@ fi
 
 #downloading images
 
-if [ $image_download == 'yes' ]; then
+if [ ! $image_download ]; then
     echo -e 'Downloading MS COCO Datasets...\n'
     
     if [ ! -d data/images ]; then
@@ -74,18 +73,21 @@ if [ $image_download == 'yes' ]; then
 fi
 
 
+cd ../..
+
 #download caption data
 
-if [ $caption_download == 'yes' ]; then
+if [ ! $caption_download ]; then
     echo -e 'Downloading MS COCO Captions...\n'
 
-    if [ ! -d data/captions/original ]; then
-        mkdir --parents data/captions/original data/captions/converted
+    if [ ! -d data/captions ]; then
+        mkdir --parents data/captions/original data/captions/converted data/captions/processed
     fi
 
     cd data/captions
 
     #download official captions
+    # annotations for test data is not offered
     wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
     unzip -l annotations_trainval2014.zip
 
