788a47429a5308636f0abb5e2884a80891f4b4bd
matasukef <f-e@toki.waseda.jp>
2017-10-17 13:09:29 +0900

    old ones

---
+++ src/CNN/resnet/resnet_feature_extractor.py
@@ -0,0 +1,9 @@
+import os
+import argparse
+import chainer
+from chainer import links as L
+from chainer import functions as F
+
+parser = ArgumentParser()
+paraser.add_argument('--input_dir', '-id', type=str, default)
+parser.add_argument('--output_dir', '-od', default)
